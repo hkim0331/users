212b7feb25d93c058340bd31012116b43bef3328
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-09-19 23:20:16 +0900

    buddy/hashers

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject users "0.1.1"
+(defproject users "0.1.2"
   :description "learn clojure/reitit and github"
   :url "not published. private product."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[environ "1.2.0"]
+  :dependencies [[buddy/buddy-hashers "1.8.1"]
+                 [environ "1.2.0"]
                  [metosin/muuntaja "0.6.8"]
                  [metosin/reitit "0.5.15"]
                  [metosin/ring-http-response "0.9.3"]
