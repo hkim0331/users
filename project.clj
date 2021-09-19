0fbc30163828757d0eed4b8236f971789bbc8de3
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-09-19 22:44:05 +0900

    release 0.1.0
    under construction

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject users "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject users "0.1.0"
+  :description "clojure/reitit の練習"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
